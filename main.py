67ce959f62d579d8a64abbbeeeb3b9ba207ee9c4
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-05-26 01:47:57 -0500

    Adds json again
    Previously removed it, but need a way to seperate
    sensitiive info from program

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import smtplib
+import json
+
+with open("info.json", "r") as file:
+  data = json.load(file)
+
 
 carrier = {
   "tmobile": "@tmomail.net", 
   "at&t":"@txt.att.net",
   "verizon": "@vtext.com",
   "sprint": "messaging.sprintpcs.com"}
+
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
