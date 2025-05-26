376eab8551ae9df9f83355e468d2ee6c651981ab
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-05-26 01:42:17 -0500

    Adds carrier options
    When sending email to text you have to have
    your carriers gateway address concatenated to
    the end of the senders phone number.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import smtplib
 
+carrier = {
+  "tmobile": "@tmomail.net", 
+  "at&t":"@txt.att.net",
+  "verizon": "@vtext.com",
+  "sprint": "messaging.sprintpcs.com"}
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
   
-s.login("user", "password")
+s.login("user", "pass")
 message = "Subject\n\n Message"
   
 s.sendmail("sender", "reciever", message)
