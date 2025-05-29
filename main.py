e149f69bc71a7b9dab59f81fe5803b7c0f913c1c
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-05-28 21:56:01 -0500

    Adds main func
    All operations and function calls will now take
    place inside of main(). I'm going to put the smtp
    algoritm into a function of itself too

--- main.py
+++ main.py
@@ -4,46 +4,39 @@ import time
 from web_request import * 
 
 
-activate = False
 
-with open("info.json", "r") as file:
-  data = json.load(file)
+
+
 
 # Append to the end of your recipient phone number if sending email to sms
 carrier = {
   "tmobile": "@tmomail.net", 
   "at&t":"@txt.att.net",
   "verizon": "@vtext.com",
-  "sprint": "messaging.sprintpcs.com"}
+  "sprint": "@messaging.sprintpcs.com"}
+
+
+def sendmail(data: json):
+  s = smtplib.SMTP('smtp.gmail.com', 587)
+  s.starttls()
+
+  s.login(data["email"], data["password"])
+  message = "testing testing\n\n testing testing"
+
+  s.sendmail(data["email"], f'{data["number"] + carrier["tmobile"]}', message)
+
+  s.quit()
 
 
 
-on = False
-
-print("type in minutes when you'd like a notification to be sent (greater than 0)")
-
-start = float(input()) * 60
-
-if start > 0:
-  on = True
-else:
-  print("type in minutes when you'd like a notification to be sent (greater than 0)")
-  start = input() * 60
-
-while on:
-  currenttime = time.time()
-
-  if currenttime >= start:
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-  
-    s.login(data["email"], data["password"])
-    message = "Testing Testing"
-
-    s.sendmail(data["email"], f'{data["number"] + carrier["tmobile"]}', message)
-
-    s.quit()
-    exit
+def main() :
+  with open("info.json", "r") as file:
+    data = json.load(file)
 
 
 
+
+
+
+if __name__ == 'main': 
+  main()
