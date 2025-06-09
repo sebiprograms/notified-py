5c1ab1a8a7912179b8a50007434b34e16ae2367e
Sebastian Stockton <96149632+sebiprograms@users.noreply.github.com>
2025-06-08 19:52:22 -0500

    Update: Notified_slack class created
    This class packages all the necessary calls to
    build a slack app and let users create there own
    custome events using the bolt event api. This
    class uses sockets not http request handling

--- notified_py/platforms/slack_bot.py
+++ notified_py/platforms/slack_bot.py
@@ -7,21 +7,32 @@ import json
 with open('info.json', 'r') as file:
     data = json.load(file)
 
-# Initilization
-app = App(token=data['bot_user_auth'],)
+class Notified_slack:
+    """
+    Creates a slack bot, event handling can be created
+    by
+    @name_of_object.app.event()
+        def func():
+            event handling here
+    """
+    with open('info.json', 'r') as file:
+        data = json.load(file)
+    def __init__(self):
+        self.app = App(token=data['bot_user_auth'])
+    def start(self):
+        handler = SocketModeHandler(self.app, data['xapp'])
+        handler.start()
+        # Allows socket connection so web requests don't 
+        # need to be handled manually
 
-
-# Allows socket connection so web requests don't 
-# need to be handled manually
-handler = SocketModeHandler(app, data['xapp'])
+# Creates 
+bot = Notified_slack()
 
 # Any eventlisteners or middleware goes here
-@app.event(event="app_mention")
+@bot.app.event(event="app_mention")
 def test_mention():
     print("bot was mentioned in Slack")
 
-
-handler.start()
-
+bot.start()
 
 
